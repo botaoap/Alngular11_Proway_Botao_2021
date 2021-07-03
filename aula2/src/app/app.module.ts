0820b348abaae9015c7d95a0c040b7986ba6acc6
lordefps <gabrielbotaoap@gmail.com>
2021-07-03 16:11:43 -0300

    AUla 2 using FormsModule to create a form

--- aula2/src/app/app.module.ts
+++ aula2/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { AngularTemplateComponent } from './angular-template/angular-template.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AngularTemplateComponent
+    AngularTemplateComponent,
+    ConsultaComponent,
+    CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Import FormsModule to use in cadastro.component.html form
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
