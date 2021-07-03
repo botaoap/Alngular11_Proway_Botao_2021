9c4a46a0ef635c617cae7264facca105abec253a
lordefps <gabrielbotaoap@gmail.com>
2021-07-03 17:53:59 -0300

    AUla 2 Angular Routes

--- aula2/src/app/app.module.ts
+++ aula2/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 
 import { AppComponent } from './app.component';
 import { AngularTemplateComponent } from './angular-template/angular-template.component';
 import { ConsultaComponent } from './consulta/consulta.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+
+const appRoutes: Routes=[
+  {path: 'angularIfElse', component:AngularTemplateComponent},
+  {path: 'angularLoopFor', component:ConsultaComponent},
+  {path: 'angularForm', component:CadastroComponent},
+];
 
 @NgModule({
   declarations: [
@@ -17,7 +24,9 @@ import { CadastroComponent } from './cadastro/cadastro.component';
   imports: [
     BrowserModule,
     // Import FormsModule to use in cadastro.component.html form
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
