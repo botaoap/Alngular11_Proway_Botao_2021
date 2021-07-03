3d850c0b9c46a6b2d0673aa2fc35e8012366fdf9
lordefps <gabrielbotaoap@gmail.com>
2021-07-03 13:38:34 -0300

    Criando um cinema com Angular

---
+++ cinemaAngular/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CinemaComponent } from './cinema/cinema.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CinemaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
