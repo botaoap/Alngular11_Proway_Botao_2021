19e241236fad7e8d257201370023ab13c56fbbf5
lordefps <gabrielbotaoap@gmail.com>
2021-07-10 15:40:33 -0300

    Aula 3 add router module in project

--- aula3/src/app/app.module.ts
+++ aula3/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 // Using library Http to use API
