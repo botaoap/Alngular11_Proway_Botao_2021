c7bf290606337c46e775d13a8cc7d4fbbc6bf014
lordefps <gabrielbotaoap@gmail.com>
2021-07-17 15:06:08 -0300

    Aula 4 Import libraries to connect to API

--- aula4/src/app/app.module.ts
+++ aula4/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ApiConnectionComponent } from './api-connection/api-connection.component';
+
+const navRoutes: Routes = [
+  {path: 'conecta-api', component: ApiConnectionComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ApiConnectionComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    // forRoot faz funcionar as Rotas
+    RouterModule.forRoot(
+      navRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
