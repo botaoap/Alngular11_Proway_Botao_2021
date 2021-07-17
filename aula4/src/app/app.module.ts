18107bf2d1d7a73b38d79e7591cb34952a1be6d5
lordefps <gabrielbotaoap@gmail.com>
2021-07-17 16:24:03 -0300

    Aula 4 Adicionando Routes

--- aula4/src/app/app.module.ts
+++ aula4/src/app/app.module.ts
@@ -7,15 +7,20 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ApiConnectionComponent } from './api-connection/api-connection.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ConsultarComponent } from './consultar/consultar.component';
+import { ExcluirComponent } from './excluir/excluir.component';
 
 const navRoutes: Routes = [
-  {path: 'conecta-api', component: ApiConnectionComponent}
+  {path: 'conecta-api', component: ApiConnectionComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'consultar', component: ConsultarComponent},
+  {path: 'excluir', component: ExcluirComponent} 
 ]
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ApiConnectionComponent, CadastrarComponent
+    ApiConnectionComponent, CadastrarComponent, ConsultarComponent, ExcluirComponent
   ],
   imports: [
     BrowserModule,
