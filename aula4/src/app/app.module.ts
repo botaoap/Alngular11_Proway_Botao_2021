a48daf883c2f79fde0c1ef0028367444f8d65223
lordefps <gabrielbotaoap@gmail.com>
2021-07-17 18:16:33 -0300

    Aula 4 Import Fomulario

--- aula4/src/app/app.module.ts
+++ aula4/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
@@ -30,7 +31,8 @@ const navRoutes: Routes = [
     RouterModule.forRoot(
       navRoutes,
       {enableTracing: true}
-    )
+    ),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
